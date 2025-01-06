95ec01eed11b172ef04df0f59e1aef77061460de
divii15 <divyadaksha143@gmail.com>
2025-01-06 17:25:58 +0530

    My next task(3)(4)

--- Task 3 Radius.py
+++ Task 3 Radius.py
@@ -1,3 +1,9 @@
+#TASK 3
+#4)find circle area--->circle area formula is πr2(pi r square).
+#input:4
+#output:50.27
+
+
 radius = int(input("Enter the 1st value:"))
 pi = float(input("Enter the 2nd value:"))
 area = pi * radius ** 2
